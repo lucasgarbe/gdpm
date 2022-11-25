15b9f518e208b87ee8376886e1d51291be92e6f5
PhilippDahlke <Philipp_dahlke@gmx.de>
2022-11-25 22:23:01 +0100

    restructured settings for prod/dev

--- Backend/asgi.py
+++ Backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for GDPMdjango project.
+ASGI config for gdpmdjango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GDPMdjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', './settings')
 
 application = get_asgi_application()
--- Backend/urls.py
+++ Backend/urls.py
@@ -1,4 +1,4 @@
-"""GDPMdjango URL Configuration
+"""gdpmdjango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
