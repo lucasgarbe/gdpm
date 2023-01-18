6ea3d6d7a17e1154cc2d7645e2cd10172524505e
PhilippDahlke <Philipp_dahlke@gmx.de>
2023-01-19 00:05:47 +0100

    PR:
    gitignore:
    - added data / statics to gitignore
    compose:
    - added build-context
    - binded local Backend-volume for better development
    - updated compose version to latest
    - build tag moved to image ( imagename:tag )

--- Backend/docker-entrypoint.sh
+++ Backend/docker-entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+# Apply database migrations
+echo "Apply database migrations"
 python manage.py makemigrations --settings=settings.development
 python manage.py migrate --settings=settings.development
-python manage.py collectstatic --settings=settings.development --noinput
-#Uncomment for production
-#python manage.py runserver 0.0.0.0:8081 --settings=settings.production
 
-python manage.py runserver 0.0.0.0:8081 --settings=settings.development
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --settings=settings.development --noinput
+
+
+# Start server
+echo "Starting server"
+#Uncomment for production
+#python manage.py runserver 127.0.0.1:8081 --settings=settings.production
+
+python manage.py runserver 0.0.0.0:8000 --settings=settings.development
