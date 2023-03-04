7e36044a6f58acb8f06b44a183c970a03a5dd3ff
PhilippDahlke <Philipp_dahlke@gmx.de>
2023-03-04 17:30:17 +0100

    register support, added Test

--- Backend/storage/admin.py
+++ Backend/storage/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Continuous, Discrete, PortSpecification, GDPM_Model
+from .models import Continuous, Discrete, PortSpecification, GDPM_Model, Support
 
 admin.site.register(Continuous)
 admin.site.register(Discrete)
 admin.site.register(PortSpecification)
 admin.site.register(GDPM_Model)
+admin.site.register(Support)
