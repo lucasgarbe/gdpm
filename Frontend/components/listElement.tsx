8d7304352f807cf5b4a0b808767026403b39fa2c
Pinie <Pinienrauschen@gmail.com>
2023-03-14 19:42:07 +0100

    GDPM-frontpage changes

---
+++ Frontend/components/listElement.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import ArrowLeft from "../components/arrow-left";
+import { ArrowUpOnSquareIcon } from '@heroicons/react/24/outline';
+import { ArrowDownTrayIcon } from '@heroicons/react/24/outline';
+import { DocumentDuplicateIcon } from '@heroicons/react/24/outline';
+import { ArchiveBoxIcon } from '@heroicons/react/24/outline';
+import { TrashIcon } from '@heroicons/react/24/outline';
+
+export default function ListElement() {
+
+    return (
+        <div className="bg-gray-100 w-full flex justify-between px-14 py-10">
+            <p className="text-2xl font-bold">Modellname</p>
+          <div className="flex gap-4">
+            <ArrowUpOnSquareIcon className="h-8 w-8"/>
+            <ArrowDownTrayIcon className="h-8 w-8"/>
+            <DocumentDuplicateIcon className="h-8 w-8"/>
+            <ArchiveBoxIcon className="h-8 w-8"/>
+            <TrashIcon className="h-8 w-8"/>
+          </div>
+          </div>
+    )
+}// vim !!!!  NeoVim - bzw. Extension für VS Code
---
+++ Frontend/pages/models.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Link from "next/link";
+import ListElement from "../components/listElement";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>GDPM</title>
+                <meta name="description" content="Grafische Entwicklungsumgebung zur Modellierung und zum Management probabilistischer Modelle" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <header className="bg-gray-100 dark:bg-gray-900 px-4 py-1 flex justify-between items-center">
+                <h1 className="text-4xl">GDPM</h1>
+                <nav className="flex gap-6">
+                    <Link href="/edit">New Model</Link>
+                    <Link href="/login">Login</Link>
+                </nav>
+            </header>
+
+            <main className="container mx-auto flex-grow pt-40">
+            <ListElement className=""/>
+                <p className="text-6xl font-bold text-center">Grafische Entwicklungsumgebung
+              Modelle! Modelle! Modelle!
+                </p>
+            </main>
+
+
+            <footer className="text-center font-light">
+                GPDM - Projektstudium
+            </footer>
+        </>
+    )
+}
