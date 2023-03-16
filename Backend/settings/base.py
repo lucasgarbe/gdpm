0845d85830957a9691e6be7c7b159fe1d1665f40
Phili <philipp_dahlke@gmx.de>
2023-03-16 21:01:46 +0100

    first converter models, simplify settings for db

--- Backend/settings/base.py
+++ Backend/settings/base.py
@@ -24,8 +24,27 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
 
 ALLOWED_HOSTS = []
+"""
+Django settings for gdpm backend (Development)
+"""
 
-# Application definition
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-&^z0q55rqn3mm@dk87y9jkx63x1caaxdq9)4^gz1%@9!pmv^x-'
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+
+}
+
+# # Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
