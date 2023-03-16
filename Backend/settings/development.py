725efdaa1670379afc568998b5d82136b5a69664
Phili <philipp_dahlke@gmx.de>
2023-03-16 21:01:46 +0100

    first converter models, simplify settings for db

---
+++ Backend/settings/development.py
@@ -0,0 +1,23 @@
+"""
+Django settings for gdpm backend (Development)
+"""
+
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-&^z0q55rqn3mm@dk87y9jkx63x1caaxdq9)4^gz1%@9!pmv^x-'
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'models': {
+        'ENGINE': 'djongo',
+        'NAME': 'db_mongo_models',
+    }
+
+}
