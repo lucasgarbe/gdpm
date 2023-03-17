fba2f964a45f3f8d18cf2ecc92e02120ae14fd8b
PhilippDahlke <Philipp_dahlke@gmx.de>
2023-03-17 19:43:22 +0100

    pymc converter for root nodes

--- Backend/converter/models.py
+++ Backend/converter/models.py
@@ -1,37 +1,50 @@
-from django.db import models
 import json
 
 
-class Distribution(object):
-    def __init__(self, name):
-        self.name = name
+class Node(object):
+    def __init__(self, id: str, type: str, data: dict):
+        self.type = type
+        self.id = id
+        self.data = data
 
 
-class Edges(object):
-    def __init__(self, name, source, target):
-        self.name = name
+class Edge(object):
+    def __init__(self, source: Node, target: Node, targetHandle: str):
         self.source = source
         self.target = target
+        self.targetHandle = targetHandle
 
 
-class Nodes(object):
-    def __init__(self, name, root, dist, edges):
-        self.name = name
-        self.root = root
-        self.dist = Distribution(**dist)
-        self.edges = Edges(**edges)
+def __convert_to_pymc(node, edges, root=False):
+    if not root:
+        arguments = 0
+
+    dist_name = node.data['dist']['name']
+    var_name = [edge.targetHandle for edge in edges if edge.source == node]
+
+    final_string = f'{node.id}_{var_name[0]} = pm.{dist_name}("{node.id}_{var_name[0]}")'
+    print(final_string)
+    return final_string
 
 
-def importJSON(js):
-    j = json.loads(js)
-    print(j)
-    # u = Nodes(**j)
-    # print(u)
+def convert_model_to_pymc(path):
+    with open(path) as f:
+        data = json.load(f)
+        nodes_dict = {
+            node["id"]:
+                Node(
+                    node["id"],
+                    node["type"],
+                    node["data"]) for node in data["nodes"]}
 
+        edges = [
+            Edge(
+                nodes_dict[edge["source"]],
+                nodes_dict[edge["target"]],
+                edge["targetHandle"]) for edge in data["edges"]]
 
-def writeRootNodes():
-    pass
+        for k, v in nodes_dict.items():
+            if v.data['root']:
+                __convert_to_pymc(v, edges, root=True)
 
-
-def writeGraph():
-    pass
+        return edges, list(nodes_dict.values())
--- Backend/converter/tests.py
+++ Backend/converter/tests.py
@@ -1,15 +1,32 @@
 import json
 from django.test import TestCase
 
-# Create your tests here.
-from .models import Nodes
+from Backend.converter.models import convert_with_decoder
 
 
 class SupportTestCase(TestCase):
 
-    def testJSON(self):
+    def test_JSON(self):
         with open('./converter/testmodel.json') as f:
             j = json.load(f)
             # u = Nodes(**j)
 
             print(j)
+
+    def test_pymc(self):
+        import pymc as pm
+        basic_model = pm.Model()
+
+        with basic_model as schools:
+            const1 = 0
+            const2 = 10
+            const3 = 1
+
+            alpha_node1 = pm.Normal("alpha", mu=const1, sigma=const2)
+            beta = pm.Normal("beta", mu=const1, sigma=const2)
+            sigma = pm.HalfNormal("sigma", sigma=const3)
+
+            obs = pm.Normal("obs", alpha=alpha_node1, beta=beta, sigma=sigma)
+
+    def convert_model_to_pymc(self):
+        convert_with_decoder('./converter/testedge.json')
