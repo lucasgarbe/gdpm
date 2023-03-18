5e59758ac4fabe453ecbd831189e57bb597eba68
Pinie <Pinienrauschen@gmail.com>
2023-03-18 21:57:08 +0100

    GDPM-45 hover effects and links

--- Frontend/pages/index.tsx
+++ Frontend/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
+import { ArrowUpTrayIcon } from '@heroicons/react/24/outline';
+import { DocumentPlusIcon } from '@heroicons/react/24/outline';
 
 export default function Home() {
     return (
@@ -18,45 +20,40 @@ export default function Home() {
                 </nav>
             </header>
 
-            <main className="flex-grow pt-40">
-            <div className="container mx-auto flex-grow pt-40">
-                <p className="text-6xl font-bold text-left text-amber-500">Grafische</p>
-                <p className="text-6xl font-bold text-left text-amber-500">Entwicklungsumgebung</p>
-                <p className="text-6xl font-bold text-left">zur Modellierung und</p>
-                <p className="text-6xl font-bold text-left">zum Management</p>
-                <p className="text-6xl font-bold text-left text-amber-500"> probabilistischer Modelle</p>
-                <div className="h-32"><p className="text-right mb-5">Beschreibungstext</p></div>
-                </div>
+            <main className="flex-grow pt-34">
+            <div className="container mx-auto pt-40">
+            <div className="group">
+                <p className="text-6xl font-bold text-left text-amber-500 group-hover:text-black">Grafische</p>
+                <p className="text-6xl font-bold text-left text-amber-500 group-hover:text-black">Entwicklungsumgebung</p>
+            </div>
+              <div className="group">
+                <p className="text-6xl font-bold text-left group-hover:text-amber-500">zur Modellierung und</p>
+                <p className="text-6xl font-bold text-left group-hover:text-amber-500">zum Management</p>
+              </div>
+              <div className="group">
+                <p className="text-6xl font-bold text-left text-amber-500 hover:text-black"> probabilistischer Modelle</p>
+              </div>
+                <div className="h-32"></div>
+              </div>
 
-                <div className="
-                h-64  w-full
-                  bg-amber-500">
-                  <div className="container mx-auto flex justify-between py-6">
-                    <div className="w-1/2">
-                    <img src="https://via.placeholder.com/150"/></div>
-                    <div  className="w-1/2">
-                      <p className="text-2xl font-bold">probabilistische Modelle erstellen</p>
+                <div className="w-full bg-amber-500">
+                  <div className="container mx-auto">
+                    <div className="flex justify-center gap-4 py-10">
+                    <Link href="/edit" className="hover:bg-black hover:text-amber-500 flex gap-4 py-2 px-3">
+                      <DocumentPlusIcon className="h-8 w-8"/>
+                      <p className="text-2xl font-bold hover:bg-black hover:text-amber-500">probabilistische Modelle erstellen</p>
+                      </Link>
                     </div>
                   </div>
+
+                  <div className="flex justify-center gap-4 pb-10">
+                <Link href="/models" className="hover:bg-black hover:text-amber-500 flex gap-4 py-2 px-3">
+                  <ArrowUpTrayIcon className="h-8 w-8"/>
+                  <p className="text-2xl font-bold ">Modelle verwalten</p>
+                  </Link>
                 </div>
-
-                <div className="h-64  w-full bg-zinc-200">
-                  <div className="container mx-auto">
-                    <div>Bild</div>
-                    <div className="text-2xl font-bold">Modelle verwalten</div>
-                  </div>
                 </div>
-
-
-                <div className="h-64  w-full bg-amber-500">
-                  <div className="container mx-auto">
-                    <div>Bild</div>
-                    <div className="text-2xl font-bold">probabilistische Modelle erstellen</div>
-                  </div>
-                </div>
-
             </main>
-
 
             <footer className="text-center font-light">
                 GPDM - Projektstudium
@@ -64,3 +61,4 @@ export default function Home() {
         </>
     )
 }
+// nur die Buchstaben der Überschrift hoverbar
