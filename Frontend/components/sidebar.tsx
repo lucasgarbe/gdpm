3f6c7669e6f8398dba9b4ae8f26a20bd7e49266d
Lucas Garbe <hi@lucasgarbe.de>
2023-03-18 15:23:16 +0100

    fetch distribution from api

--- Frontend/components/sidebar.tsx
+++ Frontend/components/sidebar.tsx
@@ -1,56 +1,65 @@
 import { dehydrate, QueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 import ArrowLeft from "../components/arrow-left";
-import { fetchDistributions, useDistributions } from "../hooks/useDistributions";
+import {
+  fetchDistributions,
+  useDistributions,
+} from "../hooks/useDistributions";
 
 interface SidebarProps {
-    className: string;
+  className: string;
 }
 
 export default function Sidebar({ className }: SidebarProps) {
-    const { data, isLoading } = useDistributions();
+  const { data, isLoading } = useDistributions();
 
-    const onDragStart= (event: any, nodeType: string, dist: Object): void => {
-        if (event.dataTransfer === null) return;
-        event.dataTransfer.setData('application/reactflow', JSON.stringify({type: nodeType, dist: dist}));
-        event.dataTransfer.effectAllowed = 'move';
-    };
+  const onDragStart = (event: any, nodeType: string, dist: Object): void => {
+    if (event.dataTransfer === null) return;
+    event.dataTransfer.setData(
+      "application/reactflow",
+      JSON.stringify({ type: nodeType, dist: dist })
+    );
+    event.dataTransfer.effectAllowed = "move";
+  };
 
-    if (isLoading) return <div>Loading</div>
+  if (isLoading) return <div>Loading</div>;
 
-    return (
-        <div className={`${className} bg-gray-100 dark:bg-gray-900 h-full p-2`}>
-            <div className="flex gap-4 items-center mb-4">
-                <Link href="/"><ArrowLeft /></Link>
-                <h1 className="text-2xl">Modelname</h1>
-            </div>
+  return (
+    <div className={`${className} bg-gray-100 dark:bg-gray-900 h-full p-2`}>
+      <div className="flex gap-4 items-center mb-4">
+        <Link href="/">
+          <ArrowLeft />
+        </Link>
+        <h1 className="text-2xl">Modelname</h1>
+      </div>
 
-
-            <div className="node-list flex flex-col gap-4">
-                {data?.map((dist, index) => (
-                    <div
-                        className="node cursor-pointer p-1 bg-white dark:bg-black border rounded border-black text-center"
-                        onDragStart={(event) => onDragStart(event, 'distribution', dist)}
-                        draggable key={index}>
-                        {dist.name}
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
+      <div className="node-list flex flex-col gap-4">
+        {data?.map((dist, index) => (
+          <div
+            className="node cursor-pointer p-1 bg-white dark:bg-black border rounded border-black text-center"
+            onDragStart={(event) => onDragStart(event, "distribution", dist)}
+            draggable
+            key={index}
+          >
+            {dist.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export async function getStaticProps() {
-    const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
-    await queryClient.prefetchQuery({
-        queryKey: ['distributions'],
-        queryFn: () => fetchDistributions(),
-    })
+  await queryClient.prefetchQuery({
+    queryKey: ["distributions"],
+    queryFn: () => fetchDistributions(),
+  });
 
-    return {
-        props: {
-            dehydratedState: dehydrate(queryClient),
-        },
-    }
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
 }
--- Frontend/hooks/useDistributions/index.ts
+++ Frontend/hooks/useDistributions/index.ts
@@ -1,16 +1,23 @@
-import ky from 'ky-universal'
-import { useQuery } from '@tanstack/react-query'
+import ky from "ky-universal";
+import { useQuery } from "@tanstack/react-query";
 
 const fetchDistributions = async () => {
-    const parsed = await ky('/api/distributions').json();
-    return parsed;
-}
+  console.log("fetch d", process.env.NEXT_PUBLIC_API_URL);
+  const discrete = await ky(
+    `${process.env.NEXT_PUBLIC_API_URL}/discrete/`
+  ).json();
+
+  const continuous = await ky(
+    `${process.env.NEXT_PUBLIC_API_URL}/continuous/`
+  ).json();
+  return [...discrete, ...continuous];
+};
 
 const useDistributions = () => {
   return useQuery({
-    queryKey: ['distributions'],
+    queryKey: ["distributions"],
     queryFn: () => fetchDistributions(),
-  })
-}
+  });
+};
 
-export { fetchDistributions, useDistributions }
+export { fetchDistributions, useDistributions };
