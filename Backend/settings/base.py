7bc2a8a9910667a3821a3d6b7fa155834a81168f
PhilippDahlke <Philipp_dahlke@gmx.de>
2023-03-19 16:40:07 +0100

    removed corsheader error

--- Backend/settings/base.py
+++ Backend/settings/base.py
@@ -35,17 +35,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'converter',
-    'corsheaders',
+    # 'corsheaders',
     'apigateway',
     'storage',
     'rest_framework',
-    #'drf_yasg',
+    # 'drf_yasg',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
