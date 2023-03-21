de31f2d83fbaca6f313c51a1e3912af3b65f0ec4
ExpertOfPlaying <alex-yu2001@yandex.com>
2023-03-21 20:23:43 +0100

    Added pymc-Endpoint, Fixed Download-Endpoint-Bug, Fixed distType-Bug

--- Backend/converter/tests.py
+++ Backend/converter/tests.py
@@ -7,4 +7,4 @@ from Backend.converter.pymc_converter import convert_model
 class SupportTestCase(TestCase):
 
     def test_convert_model_to_pymc(self):
-        print(convert_model('./converter/test/testmodel.json'))
+        print(convert_model('Backend/converter/test/testedge.json'))
