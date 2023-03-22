09526c05779064a86eaab90c66ce971676335812
Lucas Garbe <hi@lucasgarbe.de>
2023-03-22 19:40:30 +0100

    use default reactflow state / remove zustand

--- Frontend/components/workspace.tsx
+++ Frontend/components/workspace.tsx
@@ -1,4 +1,3 @@
-import { shallow } from "zustand/shallow";
 import { useCallback, useEffect, useRef, useState } from "react";
 import ReactFlow, {
   ReactFlowProvider,
@@ -6,9 +5,12 @@ import ReactFlow, {
   Background,
   MiniMap,
   Panel,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  useReactFlow,
 } from "reactflow";
 import DistributionNode from "../components/distributionNode";
-import { useStore, selector } from "../hooks/store";
 import CustomEdge from "../components/customEdge";
 import connectionLine from "./connectionLine";
 import "reactflow/dist/base.css";
@@ -16,16 +18,10 @@ import SaveButton from "./SaveButton";
 import DeleteButton from "./DeleteButton";
 import DistributionList from "./DistributionList";
 import Link from "next/link";
-import ArrowLeft from "./arrow-left";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/router";
-import kyUniversal from "ky-universal";
 import ky from "ky-universal";
 import { useQuery } from "@tanstack/react-query";
-
-interface WorkspaceProps {
-  className: string;
-}
 
 let id = 0;
 const getId = () => `node_${id++}`;
@@ -38,47 +34,57 @@ const edgeTypes = {
   default: CustomEdge,
 };
 
-export default function Workspace({ className }: WorkspaceProps) {
+function Flow() {
   const router = useRouter();
-  const reactFlowWrapper = useRef(null);
+  const flowWrapper = useRef(null);
   const [reactFlowInstance, setReactFlowInstance] = useState<any>(null);
-  const {
-    nodes,
-    edges,
-    onNodesChange,
-    onEdgesChange,
-    onConnect,
-    addNode,
-    setNodes,
-    setEdges,
-  } = useStore(selector, shallow);
   const [modelname, setModelname] = useState("Model");
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const { setViewport } = useReactFlow();
 
   const { id } = router.query;
+  const fetchModel = async () => {
+    const model = await ky
+      .get(`${process.env.NEXT_PUBLIC_API_URL}/models/${id}`)
+      .json();
+    setNodes(model.body.nodes);
+    setEdges(model.body.edges);
+    setViewport(model.body.viewport);
+  };
+
+  const onConnect = useCallback(
+    (params) => setEdges((edges) => addEdge(params, edges)),
+    [setEdges]
+  );
+
   const { data: model, isLoading } = useQuery({
     queryKey: ["model", id],
-    queryFn: () => fetchModel(id),
+    queryFn: () => fetchModel(),
     enabled: !!id,
   });
 
-  useEffect(() => {
-    if (model) {
-      setNodes(model.body.nodes);
-      setEdges(model.body.edges);
-    }
-  }, [model]);
+  //   useEffect(() => {
+  //     if (model) {
+  //       setNodes(model.body.nodes);
+  //       setEdges(model.body.edges);
+  //       setViewport(model.body.viewport);
+  //     }
+  //   }, [model]);
 
   const onDragOver = useCallback((event: any) => {
+    console.log("drag over");
     event.preventDefault();
     event.dataTransfer.dropEffect = "move";
   }, []);
 
   const onDrop = useCallback(
     (event: any) => {
+      console.log("on drop");
       event.preventDefault();
 
-      if (reactFlowWrapper.current === null) return;
-      const wrapper = reactFlowWrapper.current as any;
+      if (flowWrapper.current === null) return;
+      const wrapper = flowWrapper.current as any;
       const reactFlowBounds = wrapper.getBoundingClientRect();
       const data = JSON.parse(
         event.dataTransfer.getData("application/reactflow")
@@ -101,8 +107,7 @@ export default function Workspace({ className }: WorkspaceProps) {
         data: { dist: data.dist },
       };
 
-      // setNodes((nds) => nds.concat(newNode));
-      addNode(newNode);
+      setNodes((nds) => nds.concat(newNode));
     },
     [reactFlowInstance]
   );
@@ -112,60 +117,60 @@ export default function Workspace({ className }: WorkspaceProps) {
   }
 
   return (
-    <div className={className} ref={reactFlowWrapper}>
-      <ReactFlowProvider>
-        <ReactFlow
-          nodes={nodes}
-          edges={edges}
-          nodeTypes={nodeTypes}
-          edgeTypes={edgeTypes}
-          onNodesChange={onNodesChange}
-          onEdgesChange={onEdgesChange}
-          onConnect={onConnect}
-          onInit={setReactFlowInstance}
-          onDrop={onDrop}
-          onDragOver={onDragOver}
-          connectionLineComponent={connectionLine}
-          fitView
+    <div className="h-full w-full" ref={flowWrapper}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onInit={setReactFlowInstance}
+        onDrop={onDrop}
+        onDragOver={onDragOver}
+        connectionLineComponent={connectionLine}
+        fitView
+      >
+        <Controls />
+        <MiniMap />
+        <Panel
+          position="top-left"
+          className="bg-gray-100 rounded flex items-start p-1"
         >
-          <Controls />
-          <MiniMap />
-          <Panel
-            position="top-left"
-            className="bg-gray-100 rounded flex items-start p-1"
-          >
-            <div className="flex items-center gap-2">
-              <Link href="/">
-                <ArrowLeftIcon className="w-5" />
-              </Link>
-              <input
-                type="text"
-                value={modelname}
-                onChange={(e) => {
-                  setModelname(e.target.value);
-                }}
-                className="bg-transparent h-min"
-              />
-            </div>
-            <DistributionList />
-          </Panel>
-          <Panel position="top-right" className="bg-gray-100 rounded flex">
-            <DeleteButton
-              reactFlowInstance={reactFlowInstance}
-              modelname={modelname}
+          <div className="flex items-center gap-2">
+            <Link href="/">
+              <ArrowLeftIcon className="w-5" />
+            </Link>
+            <input
+              type="text"
+              value={modelname}
+              onChange={(e) => {
+                setModelname(e.target.value);
+              }}
+              className="bg-transparent h-min"
             />
-            <SaveButton
-              reactFlowInstance={reactFlowInstance}
-              modelname={modelname}
-            />
-          </Panel>
-          <Background />
-        </ReactFlow>
-      </ReactFlowProvider>
+          </div>
+          <DistributionList />
+        </Panel>
+        <Panel position="top-right" className="bg-gray-100 rounded flex">
+          <DeleteButton
+            reactFlowInstance={reactFlowInstance}
+            modelname={modelname}
+          />
+          <SaveButton
+            reactFlowInstance={reactFlowInstance}
+            modelname={modelname}
+          />
+        </Panel>
+        <Background />
+      </ReactFlow>
     </div>
   );
 }
 
-const fetchModel = async (id) => {
-  return ky.get(`${process.env.NEXT_PUBLIC_API_URL}/models/${id}`).json();
-};
+export default () => (
+  <ReactFlowProvider>
+    <Flow />
+  </ReactFlowProvider>
+);
