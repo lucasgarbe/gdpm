7842b802c95cf5c33496f8f586d28df453dc0be0
Lucas Garbe <hi@lucasgarbe.de>
2024-07-12 13:12:05 +0200

    updating nextjs

--- Frontend/pages/_app.tsx
+++ Frontend/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import LocalFont from "@next/font/local";
+import LocalFont from "next/font/local";
 import {
   Hydrate,
   QueryClient,
