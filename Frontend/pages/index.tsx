dfd78f123a1eed00a1dfc49480c055ed0496f8ab
Lucas Garbe <hi@lucasgarbe.de>
2024-07-31 15:01:29 +0200

    cleanup landing page

--- Frontend/pages/index.tsx
+++ Frontend/pages/index.tsx
@@ -23,58 +23,23 @@ export default function Home() {
       <Header />
 
       <main className="flex-grow pt-34">
-        <div className="container mx-auto pt-40">
-          <div className="group">
-            <p className="text-6xl font-bold text-left text-amber-500 group-hover:text-black">
+        <div className="container mx-auto py-40 text-7xl font-semibold leading-tight">
+            <p >
               Grafische
             </p>
-            <p className="text-6xl font-bold text-left text-amber-500 group-hover:text-black">
+            <p >
               Entwicklungsumgebung
             </p>
-          </div>
-          <div className="group">
-            <p className="text-6xl font-bold text-left group-hover:text-amber-500">
+            <p>
               zur Modellierung und
             </p>
-            <p className="text-6xl font-bold text-left group-hover:text-amber-500">
+            <p>
               zum Management
             </p>
-          </div>
-          <div className="group">
-            <p className="text-6xl font-bold text-left text-amber-500 hover:text-black">
-              {" "}
+            <p>
               probabilistischer Modelle
             </p>
-          </div>
-          <div className="h-32"></div>
         </div>
-
-        <div className="flex flex-grow flex-col justify-between pt-34 bg-amber-500">
-          <div className="container mx-auto">
-            <div className="flex justify-center gap-4 py-10">
-              <Link
-                href="/models/new"
-                className="hover:bg-black hover:text-amber-500 flex gap-4 py-2 px-3"
-              >
-                <DocumentPlusIcon className="h-8 w-8" />
-                <p className="text-2xl font-bold hover:bg-black hover:text-amber-500">
-                  probabilistische Modelle erstellen
-                </p>
-              </Link>
-            </div>
-          </div>
-
-          <div className="flex justify-center gap-4 pb-10">
-            <Link
-              href="/models"
-              className="hover:bg-black hover:text-amber-500 flex gap-4 py-2 px-3"
-            >
-              <ArrowUpTrayIcon className="h-8 w-8" />
-              <p className="text-2xl font-bold ">Modelle verwalten</p>
-            </Link>
-          </div>
-        </div>
-
       </main>
 
       <footer className="text-center font-light">GPDM - Projektstudium</footer>
